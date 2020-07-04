9a6e3e87e4d1245aaf8c0cd6e259c844dc0b399c
Mahdir Sheikh <mahsheikhdir@gmail.com>
2020-07-03 19:56:27 -0700

    middle ware testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { indexPage, messagesPage } from '../controllers';
+import { indexPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
